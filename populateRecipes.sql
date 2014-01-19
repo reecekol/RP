ababd566fe7aba02b2248b4cddd05743fc837303
reecek <reecekol@gmail.com>
2014-01-19 17:02:57 -0500

    sql

---
+++ populateRecipes.sql
@@ -0,0 +1,10 @@
+
+
+
+
+INSERT INTO recipes (category_id,description,created_date,chef)
+VALUES('3','Apple Crumble',NOW(),'kreece');
+
+INSERT INTO recipes (category_id,description,created_date,chef)
+VALUES('1','Baked Beans',NOW(),'kreece');
+
