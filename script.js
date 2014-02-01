7927cecae9f218221eade5d07e3135bfffa9d298
reecek <reecekol@gmail.com>
2014-02-01 05:23:11 -0500

    cleanup

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 $(document).ready(function(){
-   
-   
+
    $("button").click(function()
     {
-	
-	
-	
 	  $.post("index.php",$("#meal").serialize()).done 
 	      ( 
 		    function() {
-			
-			
 			   $("body").append("New Recipe");
 			}
 		  )
-	  
-	  
 	  return false;
 	})
 })
